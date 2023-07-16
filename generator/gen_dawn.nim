2f0d351320aa0cbd5098af81db617b14f56e25a0
ASVIEST <aaaaaabbbbbbccscc@gmail.com>
2023-07-17 00:57:36 +0300

    code refactor and dawn support

---
+++ generator/gen_dawn.nim
@@ -0,0 +1,88 @@
+import common, helpers
+
+import pkg/[oolib, shell]
+import std/[strutils, options, os]
+
+class pub DawnGenerator:
+  var
+    settings*: GeneratorSettings
+    dawnDir*: string
+
+    webgpuHeader: string
+
+  const headerPrelude = dedent """
+    const includePaths =
+        "-I '$1/out/gen/include' " &
+        "-I '$1/include' "
+    
+    {.compile(
+        "$1/out/gen/src/dawn/dawn_proc.c", 
+        includePaths
+    ).}
+  """
+
+  func preproc* =
+    self.webgpuHeader = preprocWebgpuHeader(
+      self.webgpuHeader,
+      self.headerPrelude % self.dawnDir
+    )
+
+  proc generate: GeneratedFile =
+    shell:
+      one:
+        cd ($self.dawnDir)
+        cp scripts/standalone.gclient ".gclient"
+        gclient sync
+        gn gen "out/"
+    
+    self.webgpuHeader = readFile(
+      self.dawnDir/"out/gen/include/dawn/webgpu.h"
+    )
+
+    
+    self.preproc()
+
+    ("webgpu.nim", c2nim(self.webgpuHeader).get)
+  
+  iterator virtualFS*(): GeneratedFile=
+    yield self.generate()
+
+    walkDir(self.dawnDir/"out/gen/include/dawn").each i:
+      yield ("headers/dawn"/i.path.extractFilename, i.path.readFile)
+    
+    walkDirRec(self.dawnDir/"include/dawn").each i:
+      let folder = block:
+        let lastPart = i.parentDir.lastPathPart
+        
+        if lastPart == "dawn":
+          ""
+        else:
+          lastPart
+
+      yield ("headers/dawn"/folder/i.extractFilename, i.readFile)
+    
+    walkDirRec(self.dawnDir/"out/gen/src/dawn").each i:
+      let folder = block:
+        let lastPart = i.parentDir.lastPathPart
+        
+        if lastPart == "dawn":
+          ""
+        else:
+          lastPart
+      
+      yield ("dawn"/folder/i.extractFilename, i.readFile)
+
+# const
+#   generatorDir = currentSourcePath.parentDir
+#   dawnDir = generatorDir/"dawn"
+
+# var t = DawnGenerator(
+#   dawnDir: dawnDir,
+#   settings: GeneratorSettings(
+#     genReleaseBuild: false,
+#     generatedFolder: "dawn"
+#   )
+# )
+
+# for i in t.virtualFS:
+#   i.create("aboba")
--- generator/helpers.nim
+++ generator/helpers.nim
@@ -1,4 +1,5 @@
-import std/parseutils
+import pkg/shell
+import std/[parseutils, strutils, strformat, options, tempfiles, streams]
 
 proc skipUntil*(s: string, until: string): int=
   var tmp: string
@@ -14,3 +15,161 @@ proc cropBetween*(s: string, lBound, rBound: string): string=
     idxRight = s[idxLeft..^1].skipUntil(rBound)
 
   s[0..idxLeft] & s[idxLeft + idxRight - 1..^1]
+
+template each*(iter: untyped, i: untyped{ident}, body: untyped): untyped=
+  for i in iter:
+    body
+
+const StaticLibFormat* =
+  when defined(windows):
+    "$1.lib"
+  else:
+    "lib$1.a"
+
+const webgpuHeaderPrelude = """
+#@
+{.experimental: "codeReordering".}
+
+$1
+
+@#
+
+#$2
+#mangle SIZE_MAX "sizeof(csize_t)"
+#stdints
+
+#define WGPU_EXPORT
+
+#define WGPU_OBJECT_ATTRIBUTE
+#define WGPU_ENUM_ATTRIBUTE
+#define WGPU_STRUCTURE_ATTRIBUTE
+#define WGPU_FUNCTION_ATTRIBUTE
+#define WGPU_NULLABLE
+
+#@
+type
+  WGPUAdapter* = distinct pointer
+  WGPUBindGroup* = distinct pointer
+  WGPUBindGroupLayout* = distinct pointer
+  WGPUBuffer* = distinct pointer
+  WGPUCommandBuffer* = distinct pointer
+  WGPUCommandEncoder* = distinct pointer
+  WGPUComputePassEncoder* = distinct pointer
+  WGPUComputePipeline* = distinct pointer
+  WGPUDevice* = distinct pointer
+  WGPUExternalTexture* = distinct pointer
+  WGPUInstance* = distinct pointer
+  WGPUPipelineLayout* = distinct pointer
+  WGPUQuerySet* = distinct pointer
+  WGPUQueue* = distinct pointer
+  WGPURenderBundle* = distinct pointer
+  WGPURenderBundleEncoder* = distinct pointer
+  WGPURenderPassEncoder* = distinct pointer
+  WGPURenderPipeline* = distinct pointer
+  WGPUSampler* = distinct pointer
+  WGPUShaderModule* = distinct pointer
+  WGPUSurface* = distinct pointer
+  WGPUSwapChain* = distinct pointer
+  WGPUTexture* = distinct pointer
+  WGPUTextureView* = distinct pointer
+@#
+
+"""
+
+proc preprocWebgpuHeader*(header, prelude: string, libPragma: string = "importc"): string=
+  (webgpuHeaderPrelude % [prelude, libPragma]) &
+  header
+  .withSkipUntil("#define WGPU_ARRAY_LAYER_COUNT_UNDEFINED")
+  .cropBetween(
+    "typedef struct",
+    "typedef enum"
+  )
+  .multiReplace(
+    ("typedef void const * ", "typedef void"),
+    ("#endif // WEBGPU_H_", ""),
+
+    ("// !defined(WGPU_SKIP_PROCS)", ""),
+    ("// !defined(WGPU_SKIP_DECLARATIONS)", "")
+  )
+
+const wgpuHeaderPrelude = """
+#skipinclude
+#stdints
+#$2
+
+#@
+import webgpu
+
+$1
+
+type
+  WGPUInstanceBackend* {.size: sizeof(cint).} = enum
+    WGPUInstanceBackend_None = 0x00000000
+    WGPUInstanceBackend_Vulkan = 1 shl 1
+    WGPUInstanceBackend_Metal = 1 shl 2
+    WGPUInstanceBackend_DX12 = 1 shl 3
+    WGPUInstanceBackend_DX11 = 1 shl 4
+    WGPUInstanceBackend_GL = 1 shl 5
+
+    WGPUInstanceBackend_Secondary = 
+        WGPUInstanceBackend_GL.cint or
+        WGPUInstanceBackend_DX11.cint
+
+    WGPUInstanceBackend_BrowserWebGPU = 1 shl 6
+    WGPUInstanceBackend_Primary =
+        WGPUInstanceBackend_Vulkan.cint or
+        WGPUInstanceBackend_Metal.cint or
+        WGPUInstanceBackend_DX12.cint or
+        WGPUInstanceBackend_BrowserWebGPU.cint
+    
+    WGPUInstanceBackend_Force32 = 0x7FFFFFFF
+@#
+"""
+
+proc preprocWgpuHeader*(header, prelude: string, libPragma: string = "importc"): string=
+  (wgpuHeaderPrelude % [prelude, libPragma]) & header.cropBetween(
+    "typedef enum WGPUInstanceBackend",
+    "typedef WGPUFlags WGPUInstanceBackendFlags"
+  )
+
+
+proc c2nim*(s: string): Option[string]=
+  let
+    (inFile, path) = createTempFile("webgpugen_", ".tmp")
+
+  block:
+    let strm = newFileStream(inFile)
+    strm.write s
+    strm.close
+
+  let (outFile, resPath) = createTempFile("nimout_", ".tmp")
+
+  let o = "-o:" & resPath
+  var msg: string
+
+  shellAssign:
+    msg = c2nim ($path) --strict ($o)
+
+  if "successful" notin msg:
+    return none(string)
+
+  block:
+    var strm = newFileStream(outFile)
+    result = some(strm.readAll)
+    strm.close
+
+proc cargoBuild*(dirPath: string, release: bool = false): Option[string]=
+  var cargoOut: string
+  let r =
+    if release: "-r"
+    else: ""
+
+  shellAssign:
+    cargoOut = one:
+      cd ($dirPath)
+      cargo build ($r) --lib
+  
+  if "Finished" in cargoOut:
+    return none(string)
+  
+  some(cargoOut)
